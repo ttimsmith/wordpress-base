c33ec7ca94c25b0aeb8bba6fb4f2c7491a9043d5
Tim Smith <tim@timothybsmith.com>
2013-07-23 20:32:35 -0500

    Adding signature and body class to the body

--- header.php
+++ header.php
@@ -2,6 +2,26 @@
 <html lang="en">
 
 <head>
+
+  <!-- 
+
+  @@@@@@@@@@@@@@@@@  @@@   @@@          @@@ 
+  @@@@@@@@@@@@@@@@@  @@@   @@@@        @@@@
+         @@@         @@@   @@@ @      @ @@@
+         @@@         @@@   @@@  @@  @@  @@@
+         @@@         @@@   @@@   @@@    @@@
+         @@@         @@@   @@@          @@@
+         @@@         @@@   @@@          @@@
+         @@@         @@@   @@@          @@@
+
+  How much does a polar bear weigh? Just enough to break the ice.
+
+  Made with love in beautiful Saint Paul, Minnesota.
+
+  © 2013 Anythin’ Goes LLC and Timothy B. Smith.
+
+  -->
+
   <meta charset="utf-8" />
   <title><?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -19,3 +39,4 @@
   <!--End WP Generated Header -->
   
 </head>
+<body <?php body_class($class); ?>>
